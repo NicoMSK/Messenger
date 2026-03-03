5bc9f43df94b2020fb4f1eddcb907f434f536d2a
NicoMSK <158609660+NicoMSK@users.noreply.github.com>
2026-03-03 13:04:57 +0300

    Страница входа в чаты
    * feat: Создал страницу для выбора и входа в чат
    
    * refactor: Убрал лишнее поле выбра чатов, Переписал стили через css-in-js, добавил обработку ошибок на пустое поле
    
    * style: Переделал переменные для стилей, создал отдельный файл и сделал обычную переменную для js

---
+++ src/components/pages/login-page/LoginPage.tsx
@@ -0,0 +1,74 @@
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+
+import {
+  LoginContainer,
+  LoginForm,
+  LoginSection,
+  LoginTitle,
+  LoginWrapper,
+  LoginButton,
+} from "./login-page";
+
+export function LoginPage() {
+  const [nameLength, setNameLength] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (inputValue.trim().length === 0) {
+      setNameLength(true);
+      return;
+    }
+
+    setNameLength(false);
+    setInputValue("");
+  }
+
+  function handleNameChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    const value = event.target.value;
+
+    setInputValue(value);
+
+    if (value.trim().length > 0) {
+      setNameLength(false);
+    }
+  }
+
+  return (
+    <LoginSection>
+      <LoginContainer>
+        <LoginWrapper>
+          <LoginTitle>Добро пожаловать</LoginTitle>
+          <LoginForm
+            noValidate
+            autoComplete="off"
+            onSubmit={(e) => handleSubmit(e)}
+          >
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Введите ваше имя"
+              variant="outlined"
+              error={nameLength}
+              value={inputValue}
+              helperText={nameLength ? "Поле не может быть пустым" : ""}
+              onChange={(e) => {
+                handleNameChange(e);
+              }}
+            />
+            <Stack direction="row">
+              <LoginButton fullWidth variant="contained" type="submit">
+                Войти
+              </LoginButton>
+            </Stack>
+          </LoginForm>
+        </LoginWrapper>
+      </LoginContainer>
+    </LoginSection>
+  );
+}
---
+++ src/components/pages/styleVariables.ts
@@ -0,0 +1,7 @@
+export const COLOR = {
+  background: {
+    main: "#e9efec",
+    loginWrapper: "#f5f5f5",
+    title: "#f0f8ff",
+  },
+};
