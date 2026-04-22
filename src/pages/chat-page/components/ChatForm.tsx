7171e3fbe6e4665596a76cdd74ee546672eaed9d
Николай Варюшин <pugsassts@gmail.com>
2026-04-22 17:54:14 +0300

    feat(chat): добавить логику отправки сообщений через dispatch
    - добавить локальное состояние inputText
    - подключить useDispatch с типом AppDispatch
    - реализовать handleSend с dispatch(addMessage)
    - добавить event.preventDefault()

--- src/pages/chat-page/components/ChatForm.tsx
+++ src/pages/chat-page/components/ChatForm.tsx
@@ -6,13 +6,46 @@ import {
   ChatsMessegeButton,
   ChatsWrapperForm,
 } from "./Messages.styles";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addMessage } from "../../../store/slices/chatSlice";
+import type { AppDispatch } from "../../../store/store";
 
 export function ChatForm() {
+  const [inputText, setInputText] = useState("");
+  const dispatch: AppDispatch = useDispatch();
+
+  const handleSend = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const timeMessage = new Date().toLocaleString().substring(0, 17);
+
+    if (inputText.trim() === "") return;
+
+    dispatch(
+      addMessage({
+        id: Date.now(),
+        text: inputText,
+        author: "user-1",
+        time: timeMessage,
+      }),
+    );
+
+    setInputText("");
+  };
+
   return (
     <ChatsWrapperForm>
-      <ChatsFormMessage noValidate autoComplete="off">
+      <ChatsFormMessage
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => handleSend(e)}
+      >
         <FormControl fullWidth>
-          <OutlinedInput placeholder="Сообщение..." />
+          <OutlinedInput
+            placeholder="Сообщение..."
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
         </FormControl>
         <ChatsMessegeButton
           variant="contained"
