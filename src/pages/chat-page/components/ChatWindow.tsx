77ba43e62cedbdf07b617c529d28bb6a7fa07091
Николай Варюшин <pugsassts@gmail.com>
2026-04-22 16:56:00 +0300

    feat(chat): подключить useSelector для отображения сообщений
    - заменить локальное состояние на Redux useSelector
    - отображать сообщения из store в ChatWindow

--- src/pages/chat-page/components/ChatWindow.tsx
+++ src/pages/chat-page/components/ChatWindow.tsx
@@ -1,14 +1,28 @@
 import { ChatForm } from "./ChatForm";
 import { Messages } from "./Messages";
+import type { RootState } from "../../../store/store";
+import { useSelector } from "react-redux";
+import { WrapperChatMessage } from "./ChatWindow.style";
 
 export function ChatWindow() {
+  const messages = useSelector((state: RootState) => state.message.messages);
+
   return (
     <>
-      <Messages
-        author={"НИК нейм"}
-        time={"12:45 03.03.2026"}
-        text={"Сообщение"}
-      />
+      {messages.length === 0 ? (
+        <WrapperChatMessage>Тут нет сообщений</WrapperChatMessage>
+      ) : (
+        messages.map((mes) => (
+          <Messages
+            key={mes.id}
+            id={mes.id}
+            author={mes.author}
+            time={mes.time}
+            text={mes.text}
+          />
+        ))
+      )}
+
       <ChatForm />
     </>
   );
