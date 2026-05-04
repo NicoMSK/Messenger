8a8dcbb4f152021287de90c46dd4d4be639c91d3
Николай Варюшин <pugsassts@gmail.com>
2026-05-04 19:57:50 +0300

    fix(chat): сделал очищение поле ввода, при переключении между чатами

--- src/pages/chat-page/components/ChatWindow.tsx
+++ src/pages/chat-page/components/ChatWindow.tsx
@@ -24,7 +24,7 @@ export function ChatWindow({ chatId }: ChatProps) {
           />
         ))
       )}
-      <ChatForm chatId={chatId} />
+      <ChatForm key={chatId} chatId={chatId} />
     </>
   );
 }
