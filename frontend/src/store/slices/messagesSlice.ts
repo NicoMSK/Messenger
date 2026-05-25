c3871091c8e77fc26120fdd15c5dd52734465380
Grigorii Veinin <kage.grifon@yandex.ru>
2026-05-25 11:34:26 +0500

    feat: add setMessages action, change Message.id to string

--- frontend/src/store/slices/messagesSlice.ts
+++ frontend/src/store/slices/messagesSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 export type Message = {
-  id: number;
+  id: string;
   text: string;
   author: string;
   time: string;
@@ -21,19 +21,24 @@ export const messagesSlice = createSlice({
   name: "message",
   initialState,
   reducers: {
+    setMessages: (
+      state,
+      action: PayloadAction<{ chatId: string; messages: Message[] }>,
+    ) => {
+      const { chatId, messages } = action.payload;
+      state.messages[chatId] = messages;
+    },
     addMessage: (
       state,
       action: PayloadAction<{ chatId: string; message: Message }>,
     ) => {
       const { chatId, message } = action.payload;
-
       if (!state.messages[chatId]) {
         state.messages[chatId] = [];
       }
-
       state.messages[chatId].push(message);
     },
   },
 });
 
-export const { addMessage } = messagesSlice.actions;
+export const { setMessages, addMessage } = messagesSlice.actions;
