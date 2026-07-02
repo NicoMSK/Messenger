8a16e86f354ff919f6c58e7a39d5b57cf321c4d6
Николай Варюшин <pugsassts@gmail.com>
2026-07-02 17:02:35 +0300

    refactor: Рефакторинг авторизации, WebSocket и обработки сообщений

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,39 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ChatPage } from "./pages/chat-page/ChatPage";
 import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
 import { ProtectedRoute } from "./shared/components/ProtectedRoute";
+import { connectSocket } from "./api/socket";
+import { useEffect } from "react";
+import { useAppSelector } from "./store/store-hooks";
+import { getCurrentUserName } from "./store/selectors";
 
 export default function App() {
+  const currentUser = useAppSelector(getCurrentUserName);
+
+  useEffect(() => {
+    if (currentUser) {
+      connectSocket();
+    }
+  }, [currentUser]);
+
   return (
     <ScopedCssBaseline>
       <BrowserRouter>
         <Routes>
-          <Route path="/chats/" element={
-            <ProtectedRoute element={<ChatPage />} 
-            />} />
+          <Route
+            path="/chats/"
+            element={<ProtectedRoute element={<ChatPage />} />}
+          />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/chats/" element={<ChatPage />} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route
+            path="/"
+            element={
+              currentUser ? (
+                <Navigate to="/chats" replace />
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/chat-page/components/ChatWindow.tsx
+++ frontend/src/pages/chat-page/components/ChatWindow.tsx
@@ -1,13 +1,37 @@
 import { ChatForm } from "./ChatForm";
 import { Messages } from "./Messages";
 import { WrapperChatMessage } from "./ChatWindow.style";
-import { useAppSelector } from "../../../store/store-hooks";
+import { useAppDispatch, useAppSelector } from "../../../store/store-hooks";
 import type { ChatProps } from "../../../shared/types/chat.types";
+import {
+  subscribeToMessages,
+  unsubscribeFromMessages,
+} from "../../../api/socket";
+import { addMessage } from "../../../store/slices/messagesSlice";
+import { useEffect } from "react";
 
 export function ChatWindow({ chatId }: ChatProps) {
+  const dispatch = useAppDispatch();
   const messages = useAppSelector(
     (state) => state.message.messages[chatId] ?? [],
   );
+
+  useEffect(() => {
+    subscribeToMessages((message) => {
+      console.log("MESSAGE FROM SOCKET", message);
+      console.log("DISPATCH");
+      dispatch(
+        addMessage({
+          chatId: message.message.chatId,
+          message: message.message,
+        }),
+      );
+    });
+
+    return () => {
+      unsubscribeFromMessages();
+    };
+  }, [chatId]);
 
   return (
     <>
@@ -17,10 +41,9 @@ export function ChatWindow({ chatId }: ChatProps) {
         messages.map((mes) => (
           <Messages
             key={mes.id}
-            id={mes.id}
-            author={mes.author}
-            time={mes.time}
-            text={mes.text}
+            userName={mes.userName}
+            createdAt={mes.createdAt}
+            content={mes.content}
           />
         ))
       )}
